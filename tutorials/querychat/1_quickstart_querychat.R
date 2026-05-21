7f16cbc84b575d4b9ed1b6ede659fdcf61e1791d
Ameintser <258834293+Ameintser@users.noreply.github.com>
2026-05-21 19:12:23 +0200

    Cleaning up repository

--- tutorials/querychat/1_quickstart_querychat.R
+++ tutorials/querychat/1_quickstart_querychat.R
@@ -2,13 +2,15 @@
 library(querychat)
 library(palmerpenguins)
 
+# Add greeting tutorials/querychat/0_greeting.md
 greeting_path <- "tutorials/querychat/0_greeting.md"
-# Must be an ellmer chat object. (first run 'tutorials/querychat/0_ellmer)
+
+# The client must be an ellmer chat object (first run 'tutorials/querychat/0_ellmer)
 qc <- querychat(penguins, 
                 client = client_ollama,
                 greeting = greeting_path)
 
-# Start querychat app
+# Start querychat app with the following code
 qc$app()
-# You can also start a console
+
 
